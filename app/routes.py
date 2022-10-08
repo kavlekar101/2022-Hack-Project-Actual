a96eba3dea4b4e3354af2f2e2982c4762d4f517a
JJ Kong <20kongjj@gmail.com>
2022-10-08 14:44:12 -0400

    finished form handling

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,34 @@
 from app import app
 from flask import render_template
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
 
-@app.route('/script')
+messages = []
+@app.route('/script', methods=('GET', 'POST'))
 def script():
+	if request.method == 'POST':
+		building = request.form['building']
+		day = request.form['day']
+		startT = request.form['startT']
+		endT = request.form['endT']
+		if not building:
+			flash('Title is required!')
+		elif not day:
+			flash('Content is required!')
+		elif not startT:
+			flash("Start Time is required!")
+		elif not endT:
+			flash("End Time is required!")
+		else:
+			messages.append({'building': building, 'day': day, 'startT': startT, 'endT': endT})
+			print(messages)
+			return redirect(url_for('index'))
+
+		
+
 	return render_template('script.html')
