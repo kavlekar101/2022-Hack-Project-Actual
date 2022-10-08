d98c77a03c260ab2c95e69c5c480595818cc48ad
JJ Kong <20kongjj@gmail.com>
2022-10-08 14:09:59 -0400

    no

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from app import app
 from flask import render_template
+from flask import Flask, request, render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name + last_name
+    return render_template("form.html")
+ 
+if __name__=='__main__':
+   app.run()
