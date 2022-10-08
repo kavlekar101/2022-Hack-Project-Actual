156beb864c5eb70a9e774bf24ea1abe1b8024b97
Shravun Sunkum <shravun@Shravuns-MacBook-Pro.local>
2022-10-08 14:08:55 -0400

    Worked on input form

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/script')
+def script():
+	return render_template('script.html')
