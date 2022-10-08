fabae31dc3ddc508fddad238ac79e78a72fb8672
JJ Kong <20kongjj@gmail.com>
2022-10-08 14:16:09 -0400

    init

--- app/routes.py
+++ app/routes.py
@@ -7,21 +7,6 @@ from flask import Flask, request, render_template
 def index():
 	return render_template('index.html')
 
-<<<<<<< HEAD
-@app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
-    return render_template("form.html")
- 
-if __name__=='__main__':
-   app.run()
-=======
 @app.route('/script')
 def script():
 	return render_template('script.html')
->>>>>>> 156beb864c5eb70a9e774bf24ea1abe1b8024b97
